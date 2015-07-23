73cff3a69f732783b6883f483f328d01378636aa
wl <1506766443@qq.com>
2015-07-23 17:56:17 +0800

    11111

--- app/src/main/java/com/wl/mobail/day06_lollipopdemo/MainActivity.java
+++ app/src/main/java/com/wl/mobail/day06_lollipopdemo/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View card = findViewById(R.id.card);
